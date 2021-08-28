3891532d4660205f2298481d6b22eeb51ff87427
relativehysteria <rruziro@gmail.com>
2021-08-28 06:55:14 +0200

    env python3.8 -> env python

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python
 import json
 from urllib.request import urlopen
 
--- items.py
+++ items.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.8
+#!/usr/bin/env python
 itemList = [
     "UNIQUE_HIDEOUT",
     "T1_FARM_CARROT_SEED",
